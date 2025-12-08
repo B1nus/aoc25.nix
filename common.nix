86e690443fc15e336d6c02ef4ee2c03592c31599
Lini <Lini>
2025-12-08 18:41:50 +0100

    take back compose function since it allows for eta-reduction and DRY code

--- common.nix
+++ common.nix
@@ -1,5 +1,20 @@
-with builtins; with import <nixpkgs/lib>; rec {
+with builtins;
+with import <nixpkgs/lib>;
+rec {
         lines = strings.splitString "\n";
+        compose = functions: argument:
+                if functions == [] then
+                        argument
+                else
+                        compose (tail functions) ((head functions) argument);
+        halfString = sep: s:
+                let
+                        parts = strings.splitString sep s;
+                in
+                        {
+                        left = head parts;
+                        right = elemAt parts 1;
+                };
         readInput = path: strings.trim (readFile path);
         changeList = l: i: x: lists.take i l ++ [ x ] ++ lists.drop (i + 1) l;
         rem = a: b: a - div a b * b;
