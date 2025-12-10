d97c105cf6a9095d1ec1e9591c7dac74777e5ec8
Lini <Lini>
2025-12-10 13:32:37 +0100

    Day 7 Part 2

--- 7.nix
+++ 7.nix
@@ -2,56 +2,37 @@ with builtins;
 with import <nixpkgs/lib>;
 with import ./common.nix;
 rec {
-        grid = compose [ readInput read2dString newGrid withStarterBeam (withSplits 0) ] ./7.txt;
-        withStarterBeam = grid:
-                withBeams (toList (newPos 0 (div grid.width 2))) grid;
-        withBeams = beams: grid:
-                {
-                        inherit (grid) content width height;
-                        inherit beams;
-                };
-        withSplits = splits: grid:
-                {
-                        inherit (grid) content width height beams;
-                        inherit splits;
-                };
-        gravity = beam: with beam;
-                {
-                        row=row + 1;
-                        inherit col;
-                };
-        split = beam: with beam;
-                [
-                        (newPos row (col - 1))
-                        (newPos row (col + 1))
-                ];
-        newBeams = grid: with grid;
-                if beams == [] then
-                        []
+        cellss = compose [ readInput read2dString ] ./7.txt;
+        beams = let
+                        width = length (head cellss);
+                        zeros = replicate (div width 2) 0;
+                in
+                        zeros ++ [ 1 ] ++ zeros;
+        beamFilter = cellFilter: cell: beam:
+                if cell == cellFilter then
+                        beam
                 else
-                        let
-                                tailBeams = tail beams;
-                                beam = head beams;
-                                gravityBeam = gravity beam;
-                                cell = elemAtGrid grid gravityBeam;
-                        in
-                                if cell == null then
-                                toList beam ++ tailBeams
-                        else if cell == "^" then
-                                split gravityBeam ++ newBeams (withBeams tailBeams grid)
-                        else
-                                toList gravityBeam ++ newBeams (withBeams tailBeams grid);
-        simulateEntire = grid:
-                simulate (grid.height - 1) grid;
-        simulate = steps: grid:
-                if steps == 0 then
-                        grid
-                else 
-                        let
-                                nextBeams = (newBeams grid);
-                                newSplits = length nextBeams - length grid.beams;
-                        in
-                                simulate (steps - 1) (withSplits
-                                        (grid.splits + newSplits)
-                                        (withBeams (nub nextBeams) grid));
+                        0;
+        collidingBeams = cellRow: beamRow:
+                zipListsWith (beamFilter "^") cellRow beamRow;
+        unsplitBeams = cellRow: beamRow:
+                zipListsWith (beamFilter ".") cellRow beamRow;
+        splitBeams = cellRow: beamRow:
+                let
+                        colliding = collidingBeams cellRow beamRow;
+                in
+                        zipListsWith
+                add
+                (tail colliding ++ toList 0)
+                (toList 0 ++ colliding);
+        nextBeams = cellRow: beamRow:
+                zipListsWith
+                add
+                (splitBeams cellRow beamRow)
+                (unsplitBeams cellRow beamRow);
+        simulate = cellss: beams:
+                if cellss == [] then
+                        beams
+                else
+                        jayce beams (simulate (tail cellss) (nextBeams (head cellss) beams));
 }
